b269ce9d663ca1f412dc3431d1646d10fbaa4513
Parth Chandak <parth.chandak02@gmail.com>
2024-09-20 10:31:08 -0700

    minor fixes before ios signin - not working yet

--- app/utils/googleCalendar.ts
+++ app/utils/googleCalendar.ts
@@ -1,12 +1,13 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env';
 
 // Initialize Google Sign-In
 GoogleSignin.configure({
   scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
-  iosClientId: Platform.OS === 'ios' ? process.env.IOS_CLIENT_ID : undefined,
-  webClientId: process.env.WEB_CLIENT_ID,
+  iosClientId: Platform.OS === 'ios' ? IOS_CLIENT_ID : undefined,
+  webClientId: WEB_CLIENT_ID,
 });
 
 export async function signIn() {
