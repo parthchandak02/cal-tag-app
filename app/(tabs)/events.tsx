a23a6d9129f85d59f0bc9f20a5a603807c42f604
Parth Chandak <parth.chandak02@gmail.com>
2024-09-20 13:37:54 -0700

    minor fixes before ios signin - not working yet v4

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, FlatList, Image } from 'react-native';
 
 import ParallaxScrollView from '../../components/ParallaxScrollView';
