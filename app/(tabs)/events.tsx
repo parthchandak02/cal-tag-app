24ac6b52592b1fe36e3f650f58cdbb626caa899b
Parth Chandak <parth.chandak02@gmail.com>
2024-09-20 10:33:57 -0700

    minor fixes before ios signin - not working yet v3

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, FlatList, Image } from 'react-native';
 
 import ParallaxScrollView from '../../components/ParallaxScrollView';
---
+++ react-native-dotenv.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const IOS_CLIENT_ID: string;
+  export const WEB_CLIENT_ID: string;
+}
